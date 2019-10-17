ed1e0c95a239c1f518bc238d86096a40bbdb7cba
vovalexeev <Alexeev-1740ve@mail.ru>
2019-10-17 20:21:06 +0300

    NewClassCredit

---
+++ src/Credit.java
@@ -0,0 +1,11 @@
+public class Credit {
+    private double balance;
+    private Client client;
+
+    Credit(Client client, double amountMoney) {
+        this.client = client;
+        this.balance = amountMoney;
+    }
+
+    //some functions for Credit
+}
